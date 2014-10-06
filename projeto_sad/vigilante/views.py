4bac7e48dc0f07de04673d500454caf4b9ddef20
ciromoraismedeiros <ciromoraismedeiros@gmail.com>
2014-10-06 16:09:45 -0300

    Limpando o código

--- projeto_sad/vigilante/views.py
+++ projeto_sad/vigilante/views.py
@@ -10,14 +10,6 @@ def home(req):
         # Marcando ponto
         endereco = req.POST['endereco']
         valores['p'] = get_lat_lng(endereco)
-        
-        # Marcando áreas de risco
-        # Obs.: esses valores futuramente virão do banco de dados, 
-        # mas por enquanto estão só no código-fonte (hard-coded)
-        valores['areas'] = []
-        valores['areas'] += [-6.462477,-37.1083922, 'red'] # Odilon Lebarre, 167, Caicó - RN, Brasil
-        valores['areas'] += [-6.4621788,-37.0943734, 'blue'] # Av. Cel. Martiniano, 781, Caicó - RN, Brasil
-        valores['areas'] += [-6.4652023,-37.0942773, 'green'] # Rua Generina Vale, 1206, Caicó- RN, Brasil
     return render_to_response('home.html',
         valores,
         context_instance=RequestContext(req))
