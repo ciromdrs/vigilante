f53763e1609d11127209677da4e4df1b822c03b4
ciromoraismedeiros <ciromoraismedeiros@gmail.com>
2014-10-06 08:54:50 -0300

    Marcando um ponto atraves do endereco

--- projeto_sad/projeto_sad/urls.py
+++ projeto_sad/projeto_sad/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import vigilante
+from vigilante.views import home
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', home,),
+    url(r'^/', home,),
+    url(r'^/?P<endereco>', home,), #escrever regex
 )
--- projeto_sad/vigilante/views.py
+++ projeto_sad/vigilante/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from vigilante.util import get_lat_lng
 
 # Create your views here.
+def home(req):
+    valores = {}
+    if req.method == 'POST':
+        endereco = req.POST['endereco']
+        valores['p'] = get_lat_lng(endereco)
+    return render_to_response('home.html',
+        valores,
+        context_instance=RequestContext(req))
