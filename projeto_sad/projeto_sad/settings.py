cae05768c0bc69ea0448756e51e5a68b6ecd25ff
ciromoraismedeiros <ciromoraismedeiros@gmail.com>
2014-10-06 15:31:29 -0300

    Adicionando CSS e static files

--- projeto_sad/projeto_sad/settings.py
+++ projeto_sad/projeto_sad/settings.py
@@ -13,7 +13,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates/'),
+    os.path.join(BASE_DIR, '/vigilante/templates/'),
 )
 
 # Quick-start development settings - unsuitable for production
@@ -86,5 +86,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '/vigilante/static/'),
+)
